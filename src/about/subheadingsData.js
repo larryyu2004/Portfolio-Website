776e87314f3a166f80fe287850be19d5293bfb5a
larryyu2004 <jiayiyu@myyahoo.com>
2025-01-01 22:36:00 +1030

    homepage

---
+++ src/about/subheadingsData.js
@@ -0,0 +1,78 @@
+const subheadingsData = {
+    1: [
+        {
+            title: "About Me",
+            content: (
+                <>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                </>
+            )
+        }
+    ],
+
+    2: [
+        {
+            title: "2024 Bachelor Degree",
+            content: (
+                <>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                </>
+            )
+        },
+        {
+            title: "2023 Bachelor Degree",
+            content: (
+                <>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                </>
+            )
+        }
+    ],
+
+    3: [
+        {
+            title: "2022 Front end Developer",
+            content: (
+                <>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                </>
+            )
+        },
+        {
+            title: "2020 OS Designer",
+            content: (
+                <>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                </>
+            )
+        },
+        {
+            title: "2017 CPU Designer",
+            content: (
+                <>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                </>
+            )
+        }
+    ]
+}
+
+export default subheadingsData;
---
+++ src/playerStats/PlayerStats.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/playerStats.css";
+
+const Playstats =() => {
+    return (
+    <div id = "playerStats">
+        <h1> Larry YU LEVEL 20</h1>
+        <div id = "playerStats__lines">
+            <span
+            id = "playerStats__lines__thick"></span>
+            <span
+            id = "playerStats__lines__thin"></span>
+        </div>
+        <h2>Fullstack Developer</h2>
+    </div>
+    )
+}
+
+export default Playstats;
